5d9e51766c6dedb5044462338748236d2621239c
Kairix <kheira-deborah.kemmoun@epitech.eu>
2022-06-10 22:55:45 +0200

    feat: add token on local storage

--- src/index.tsx
+++ src/index.tsx
@@ -1,11 +1,24 @@
 import React from 'react';
 
+import { debounce } from '@mui/material';
+import axios from 'axios';
 import ReactDOM from 'react-dom/client';
 import { Provider as StoreProvider } from 'react-redux';
 
+import { saveStateToken, LocalStorageKey } from '@helpers/localStorage';
+
 import { App } from './App';
 import { store } from './store/store';
+
 import './index.css';
+
+axios.defaults.baseURL = 'https://workee-back.brangers.eu';
+
+store.subscribe(
+  debounce(() => {
+    saveStateToken(store.getState().user.token, LocalStorageKey.token);
+  }, 800)
+);
 
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
 root.render(
