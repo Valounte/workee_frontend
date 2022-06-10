35d1cd7f268ad2ef4e8caf5dfb4ed82cfae98ad4
Kairix <kheira-deborah.kemmoun@epitech.eu>
2022-06-10 10:37:38 +0200

    feat: adding redux toolkit ton app

--- src/index.tsx
+++ src/index.tsx
@@ -1,13 +1,17 @@
 import React from 'react';
 
 import ReactDOM from 'react-dom/client';
+import { Provider as StoreProvider } from 'react-redux';
 
-import './index.css';
 import { App } from './App';
+import { store } from './store/store';
+import './index.css';
 
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
 root.render(
   <React.StrictMode>
-    <App />
+    <StoreProvider store={store}>
+      <App />
+    </StoreProvider>
   </React.StrictMode>
 );
