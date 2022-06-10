a1d72b3811040a53ec0d5e47e0a796a050ed87ef
Kairix <kheira-deborah.kemmoun@epitech.eu>
2022-06-10 17:17:48 +0200

    feat: add alias for entities

--- src/entity/user/store/selectors/selectToken.selector.ts
+++
@@ -1,8 +0,0 @@
-import { createSelector } from '@reduxjs/toolkit';
-
-import { RootState } from 'src/store/store';
-
-export const selectToken = createSelector(
-  (state: RootState) => state.user,
-  entity => entity.token
-);
