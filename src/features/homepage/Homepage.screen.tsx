44df5be405065c930a9d65245ecc5cd5bc41b988
jessiichat <jessiichat@gmail.com>
2022-07-05 23:27:08 +0200

    style: wireframed homepage screen

--- src/features/homepage/Homepage.screen.tsx
+++ src/features/homepage/Homepage.screen.tsx
@@ -2,12 +2,16 @@ import React from 'react';
 
 import { Container } from '@ui-kit';
 
+import { About } from './features/About';
 import { BrandStatistics } from './features/BrandStatistics';
 import { HeroHeader } from './features/HeroHeader';
+import { Product } from './features/Product';
 
 export const HomePageScreen = () => (
-  <Container>
+  <Container maxWidth="md">
     <HeroHeader />
     <BrandStatistics />
+    <About />
+    <Product />
   </Container>
 );
