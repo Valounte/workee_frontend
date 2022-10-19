f700bdf2557f7875682e5bbe6d43689d9d2693be
Jessica Chapuis <jessiichat@gmail.com>
2022-10-19 23:44:08 +0200

    style: add main navigation component and improve ui (wip)

--- src/app/usersHandler/UsersHandler.screen.tsx
+++ src/app/usersHandler/UsersHandler.screen.tsx
@@ -4,11 +4,22 @@ import { useSelector } from 'react-redux';
 import { useNavigate } from 'react-router-dom';
 import { useAsync } from 'react-use';
 
+import { MainNavigation } from '@common-features/main-navigation/MainNavigation';
 import { selectToken } from '@entities/authentification/store/selectors/selectToken.selector';
 import { getJobsThunk } from '@entities/jobs/store/thunks/getJobs.thunk';
 import { getTeamsThunk } from '@entities/teams/store/thunks/getTeams.thunk';
 import { getUsersThunk } from '@entities/users/store/thunks/getUsers.thunk';
-import { Typography, Tabs, Tab, Box, TabPanel } from '@ui-kit';
+import {
+  Typography,
+  Tabs,
+  Tab,
+  Box,
+  TabPanel,
+  Stack,
+  Container,
+  styled,
+  Divider,
+} from '@ui-kit';
 
 import { MainRoutesEnum } from '../../RoutesEnum';
 import { useAppDispatch } from '../../store/useAppDispatch';
@@ -26,6 +37,19 @@ enum TabsEnum {
   jobs = 'jobs',
 }
 /* eslint-enable no-unused-vars */
+
+const StyledContainer = styled(Container)`
+  max-width: 100% !important;
+  padding: 0 !important;
+`;
+
+const StyledTabs = styled(Tabs)`
+  border-bottom: '1px solid grey';
+`;
+
+const StyledTab = styled(Tab)`
+  margin: 6px 0 !important;
+`;
 
 const UsersHandlerScreen = () => {
   const [value, setValue] = React.useState(TabsEnum.users);
@@ -52,29 +76,45 @@ const UsersHandlerScreen = () => {
     dispatch(getUsersThunk({ token }))
   );
   return (
-    <div>
-      <Box display="flex" justifyContent="center">
-        <Tabs value={value} onChange={handleChangeTab} variant="scrollable">
-          <Tab disableRipple value={TabsEnum.users} label="Users" />
-          <Tab disableRipple value={TabsEnum.teams} label="Teams" />
-          <Tab disableRipple value={TabsEnum.jobs} label="Jobs" />
-        </Tabs>
-      </Box>
-      <TabPanel value={value} index={TabsEnum.users}>
-        <Typography>Inviter un nouvel utilisateur !</Typography>
-        <InviteUserForm />
-        <DataGridUsers loading={loadingUsers} error={errorUsers} />
-      </TabPanel>
-      <TabPanel value={value} index={TabsEnum.teams}>
-        <Typography>Teams</Typography>
-        <CreateTeamForm />
-        <DataGridTeams loading={loadingTeams} error={errorTeams} />
-      </TabPanel>
-      <TabPanel value={value} index={TabsEnum.jobs}>
-        <Typography>Jobs</Typography>
-        <DataGridJobs loading={loadingJobs} error={errorJobs} />
-      </TabPanel>
-    </div>
+    <StyledContainer>
+      <Stack direction="row" height="100vh" spacing={0}>
+        <MainNavigation />
+        <Box width="100vw">
+          <StyledTabs value={value} onChange={handleChangeTab}>
+            <StyledTab disableRipple value={TabsEnum.users} label="Users" />
+            <Tab disableRipple value={TabsEnum.teams} label="Teams" />
+            <Tab disableRipple value={TabsEnum.jobs} label="Jobs" />
+          </StyledTabs>
+          <Box p={4}>
+            <TabPanel value={value} index={TabsEnum.users}>
+              <Typography variant="h5" mb={2}>
+                Inviter un nouvel utilisateur !
+              </Typography>
+              <Stack
+                direction="row"
+                spacing={2}
+                divider={<Divider orientation="vertical" flexItem />}>
+                <InviteUserForm />
+                <DataGridUsers loading={loadingUsers} error={errorUsers} />
+              </Stack>
+            </TabPanel>
+            <TabPanel value={value} index={TabsEnum.teams}>
+              <Typography variant="h5" mb={2}>
+                Teams
+              </Typography>
+              <CreateTeamForm />
+              <DataGridTeams loading={loadingTeams} error={errorTeams} />
+            </TabPanel>
+            <TabPanel value={value} index={TabsEnum.jobs}>
+              <Typography variant="h5" mb={2}>
+                Jobs
+              </Typography>
+              <DataGridJobs loading={loadingJobs} error={errorJobs} />
+            </TabPanel>
+          </Box>
+        </Box>
+      </Stack>
+    </StyledContainer>
   );
 };
 
